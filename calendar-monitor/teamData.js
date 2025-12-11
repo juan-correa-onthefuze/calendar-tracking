0a6b741ba51711dd504149dad0132d247963262d
juan-correa-onthefuze <75704844+juan-correa-onthefuze@users.noreply.github.com>
2025-12-11 15:05:51 -0500

    update team

--- calendar-monitor/teamData.js
+++ calendar-monitor/teamData.js
@@ -27,7 +27,40 @@ const TEAM_MEMBERS = [
   { name: 'Nicolás González Díaz', email: 'nicolas.gonzalez@onthefuze.com', role: 'Software' },
   { name: 'Norman Eduardo Jaimes Mora', email: 'norman.jaimes@onthefuze.com', role: 'Software' },
   { name: 'Pablo Andres Muñoz Ávila', email: 'pablo.munoz@onthefuze.com', role: 'Software' },
-  { name: 'Viviana Romero', email: 'viviana.romero@onthefuze.com', role: 'Software' }
+  { name: 'Viviana Romero', email: 'viviana.romero@onthefuze.com', role: 'Software' },
+
+  
+  // Consultants
+  { name: 'Arturo Herrera', email: 'arturo.herrera@onthefuze.com', role: 'Consultant' },
+  { name: 'Daniel Lastra', email: 'daniel.lastra@onthefuze.com', role: 'Consultant' },
+  { name: 'Maria Agudelo', email: 'maria.agudelo@onthefuze.com', role: 'Consultant' },
+  { name: 'Jose Salgado', email: 'jose.salgado@onthefuze.com', role: 'Consultant' },
+  { name: 'Lizeth Pinzon', email: 'lizeth.pinzon@onthefuze.com', role: 'Consultant' },
+  { name: 'Daniel Carrasco', email: 'daniel.carrasco@onthefuze.com', role: 'Consultant' },
+  { name: 'Valery Puentes', email: 'valery.puentes@onthefuze.com', role: 'Consultant' },
+  { name: 'Guido Torres', email: 'guido.torres@onthefuze.com', role: 'Consultant' },
+  { name: 'Sebastian Camacho', email: 'sebastian.camacho@onthefuze.com', role: 'Consultant' },
+  { name: 'Jeisson Zambrano', email: 'jeisson.zambrano@onthefuze.com', role: 'Consultant' },
+  { name: 'Tatiana Rosas', email: 'tatiana.rosas@onthefuze.com', role: 'Consultant' },
+  { name: 'Camilo Sarmiento', email: 'camilo.sarmiento@onthefuze.com', role: 'Consultant' },
+  { name: 'Sarah Ortiz', email: 'sarah.ortiz@onthefuze.com', role: 'Consultant' },
+  { name: 'Maria Becerra', email: 'maria.becerra@onthefuze.com', role: 'Consultant' },
+  { name: 'Manuel Wilches', email: 'manuel.wilches@onthefuze.com', role: 'Consultant' },
+  { name: 'Sebastian Laguna', email: 'sebastian.laguna@onthefuze.com', role: 'Consultant' },
+  { name: 'David Cabrera', email: 'david.cabrera@onthefuze.com', role: 'Consultant' },
+  { name: 'Daniela Gamba', email: 'daniela.gamba@onthefuze.com', role: 'Consultant' },
+  { name: 'Julian Rubio', email: 'julian.rubio@onthefuze.com', role: 'Consultant' },
+  { name: 'Karen Cely', email: 'karen.cely@onthefuze.com', role: 'Consultant' },
+  { name: 'Simon Ocampo', email: 'simon.ocampo@onthefuze.com', role: 'Consultant' },
+  { name: 'Maria Soraca', email: 'maria.soraca@onthefuze.com', role: 'Consultant' },
+  { name: 'Jonatan Diaz', email: 'jonatan.diaz@onthefuze.com', role: 'Consultant' },
+  { name: 'Valentina Guzman', email: 'valentina.guzman@onthefuze.com', role: 'Consultant' },
+  { name: 'Diego Rodriguez', email: 'diego.rodriguez@onthefuze.com', role: 'Consultant' },
+  { name: 'Pablo Aristizabal', email: 'pablo.aristizabal@onthefuze.com', role: 'Consultant' },
+  { name: 'Melissa Florez', email: 'melissa.florez@onthefuze.com', role: 'Consultant' },
+  { name: 'Valeria Marin', email: 'valeria.marin@onthefuze.com', role: 'Consultant' }
+
+
 ];
 
 module.exports = TEAM_MEMBERS;
